eb01efc4700c5c0f9c91fa71c164a0abe0263f89
duynd243 <duyndse@gmail.com>
2023-08-18 02:14:18 +0700

    Change `maximumFractionDigits` of `currencyFormatter`

--- src/components/CalculatorSection.tsx
+++ src/components/CalculatorSection.tsx
@@ -6,7 +6,7 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
   minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
+  maximumFractionDigits: 2,
 });
 
 function CalculatorSection() {
